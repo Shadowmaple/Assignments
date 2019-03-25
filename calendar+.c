0dbb321665d92e5d12f626923fbaa982e985dafd
Shadowmaple <1142319190@qq.com>
2019-03-25 21:59:05 +0800

    add

---
+++ calendar+.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int day[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+    int total=2;
+    puts("2019年日历表");
+    printf("   ");
+    for (int i=0; i<12; i++){
+        for (int j=1; j<=day[i]; j++, total++) {
+            printf("%2d ", j);
+            if (total==7) {
+                putchar('\n');
+                total = 0;
+            }
+        }
+        if (total != 1) putchar('\n');
+        for (int n=1; n<total; n++) {
+            printf("   ");
+        }
+    }
+    return 0;
+}
