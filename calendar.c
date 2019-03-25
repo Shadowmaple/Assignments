dfc0fe77c0b41ff42b998f0e1746281ce9e0a903
Shadowmaple <1142319190@qq.com>
2019-03-25 22:50:17 +0800

    modify

--- calendar.c
+++ calendar.c
@@ -3,6 +3,7 @@ int main()
 {
     int day[12]={31,28,31,30,31,30,31,31,30,31,30,31};
     int total=1;
+    printf("2019年日历表\n");
     for (int i=0; i<12; i++) {
         printf("%d月\n", i+1);
         for (int j=1; j<=day[i]; j++) {
