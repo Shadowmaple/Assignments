9ecf409d5b15123382e452d782361850ec472675
Shadowmaple <1142319190@qq.com>
2019-04-18 15:16:19 +0800

    update

--- C_lang_course/multiply_longInt.c
+++ C_lang_course/multiply_longInt.c
@@ -5,7 +5,7 @@
 
 #define N 30
 
-//清楚字符串前面的0
+//清除字符串前面的'0'
 void clear_zero(char s[])
 {
     int i = 0;
@@ -22,7 +22,7 @@ void add_number(char c[], char n[])
         int num = (n[i] - '0') + (c[j] - '0') + more;
         //进位
         more = num / 10;
-        //c中原来的数字改变
+        //相加后该位的结果
         c[j] = num % 10 + '0';
     }
 }
@@ -32,13 +32,10 @@ void multiply_number(int num, char b[], char n[], int index)
     int more = 0;
     for (int i = strlen(b)-1; i>=0; i--, index--) {
         int mul = num * (b[i] - '0') + more;
-//        printf("mul=%d num=%d b[%d]=%c\n", mul, num, i, b[i]);
         //获取十位数，即进位数
         more = mul / 10;
-        //获取个位数，即保留数
-        int m = mul % 10;
-        n[index] = m + '0';
-//        printf("n[%d]=%d more=%d\n", index, m, more);
+        //个位数，赋值给n[index]
+        n[index] = mul % 10 + '0';
     }
 
     //多出的进位
@@ -49,24 +46,23 @@ int main()
 {
     char a[N+1], b[N+1], c[2*N+1];
     
-    printf("a=");
+    printf("a = ");
     scanf("%s", a);
-    printf("b=");
+    printf("b = ");
     scanf("%s", b);
     //初始化，各字符为0
     memset(c, '0', 2*N+1);
 
-    //从各位开始乘
+    //从a的各位开始乘
     for (int i = strlen(a)-1; i >= 0; i--) {
-        //转换为整数
         int num = a[i] - '0';
         int zero_num = strlen(a) - i - 1;
+        //a和b字符串的长度之和
         int len_ab = strlen(a) + strlen(b);
 
         //存储数与另一位长整数相乘的字符串
         char n[len_ab];
         memset(n, '0', len_ab+1);
-//        printf("%d %s %ld\n", len_ab, n, strlen(n));
         
         int index = len_ab - zero_num - 1;
 
@@ -80,7 +76,7 @@ int main()
     }
 
     clear_zero(c);
-    printf("a*b=%s\n", c);
+    printf("a * b = %s\n", c);
 
     return 0;
 }
