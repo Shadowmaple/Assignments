ac974aee931879736e9d40f5c6df1c97293e0d9c
Shadowmaple <1142319190@qq.com>
2019-05-22 20:18:07 +0800

    modify

--- C_lang_course/output_reverse.c
+++ C_lang_course/output_reverse.c
@@ -5,11 +5,12 @@
 
 int main()
 {
-	char str[19] = "华中师范大学";
+	char str[13] = "华中师大";
 	char x[4];
-	int i, j=1;
+	int i;
 	for (i = strlen(str) - 3; i >= 0; i -= 3) {
 		strncpy(x, str + i, 3);
+		x[3] = '\0';
 		printf("%s", x);
 	}
 	putchar('\n');
