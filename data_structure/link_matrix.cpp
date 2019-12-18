28d6110407f8664444357a88370b36a34aeac607
Shadowmaple <1142319190@qq.com>
2019-12-18 08:46:59 +0800

    modify

--- data_structure/link_matrix.cpp
+++ data_structure/link_matrix.cpp
@@ -1,25 +1,33 @@
+/*************************************************************************
+题目：给定一个稀疏矩阵，编写程序将其转化为链式存储结构。
+提示：
+矩阵每行的非零元素用一个单链表存放，单链表中每个节点需要存放列信息和元素值。
+数组parray用于存放每行单链表的首地址。编写函数createLinkMatrix根据矩阵创建链式结构，
+函数displayLinkMatrix以[行、列、值]格式显示链式矩阵内容。
+**************************************************************************/
+
 # include <iostream>
 using namespace std;
 
-struct SingelLink {
+struct SingleNode {
     int column;
     int value;
-    SingelLink *next;
+    SingleNode *next;
 };
 
 struct LinkMatrix {
-    SingelLink *link;
+    SingleNode *link;
 };
 
 void createLinkMatrix(LinkMatrix *matrix, int *num, int row, int column) {
     for (int i = 0; i < row; i++) {
-        SingelLink *currentLink = matrix[i].link;
-        currentLink = NULL;
+        matrix[i].link = nullptr;
+        SingleNode *currentLink = matrix[i].link;
 
         for (int j = 0; j < column; j++) {
             int x = i * column + j;
             if (!num[x]) continue;
-            SingelLink *link = new SingelLink{j, num[x], NULL};
+            SingleNode *link = new SingleNode{j, num[x], NULL};
             if (currentLink == nullptr) {
                 matrix[i].link = link;
                 currentLink = link;
@@ -31,17 +39,17 @@ void createLinkMatrix(LinkMatrix *matrix, int *num, int row, int column) {
     }
 }
 
-// bug：行中全为0时出现段错误，不全为0正常
 void displayLinkMatrix(LinkMatrix *matrix, int row) {
+    int count = 0;
     for (int i = 0; i < row; i++) {
-        cout << i;
-        SingelLink *link = matrix[i].link;
+        SingleNode *link = matrix[i].link;
         while (link) {
-            printf("\t%d, %d", link->column, link->value);
+            printf("[%d, %d, %d]\n", i, link->column, link->value);
             link = link->next;
+            count++;
         }
-        cout << '\n';
     }
+    printf("共%d个节点\n", count);
 }
 
 int main() {
@@ -50,7 +58,7 @@ int main() {
                     -1, -2, 0, 0, 0,
                     0, 0, 0, 0, 0,
                     0, 0, 0, 2, 0};
-    LinkMatrix matrix[5];
+    LinkMatrix matrix[5] = {NULL};
     createLinkMatrix(matrix, num, 5, 5);
     displayLinkMatrix(matrix, 5);
 
