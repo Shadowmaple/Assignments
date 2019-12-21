41d231a7e79f6f4120a960008262ae43ef67e733
Shadowmaple <1142319190@qq.com>
2019-12-21 15:29:22 +0800

    modify

--- Cpp_course/practice/5.2.cpp
+++ Cpp_course/practice/5.2.cpp
@@ -6,10 +6,8 @@ class Point {
 private:
     double x, y;
 public:
-    Point(double x, double y) {
-        this->x = x;
-        this->y = y;
-    };
+    // 析构函数，初始化列表
+    Point(double a, double b):x(a), y(b) {};
     double Distance(const Point &p) {
         double a = x - p.x, b = y - p.y;
         return sqrt(a * a + b * b);
