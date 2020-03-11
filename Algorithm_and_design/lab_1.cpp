11e9fdac8405515a2f13075b65f5643249759367
Shadowmaple <1142319190@qq.com>
2020-03-11 15:43:07 +0800

    update

--- Algorithm_and_design/lab_1.cpp
+++ Algorithm_and_design/lab_1.cpp
@@ -4,19 +4,35 @@
 # include <ctime>
 using namespace std;
 
+// 产生随机数
 int proRand(int a, int b) {
     return rand() % (b-a) + a;
 }
 
-void pubSort(vector<int>& num) {
-    for (int i = 0; i < num.size()-1; i++) {
-        for (int j = i+1; j < num.size(); j++) {
+// 样本数据，k组，每组n个
+void sampleData(vector<vector<int>>& v, int k, int n) {
+    for (int i = 0; i < k; i++) {
+        vector<int> num;
+        for (int j = 0; j < n; j++) {
+            int n = proRand(0, 100);
+            num.push_back(n);
+        }
+        v.push_back(num);
+    }
+}
+
+// 冒泡排序
+void bubbleSort(vector<int>& num) {
+    int n = num.size();
+    for (int i = 0; i < n-1; i++) {
+        for (int j = i+1; j < n; j++) {
             if (num[i] > num[j]) swap(num[i], num[j]);
         }
     }
 }
 
-void quickSortPa(vector<int>& num, int left, int right) {
+// 快速排序
+void quickSort(vector<int>& num, int left, int right) {
     if (left > right) return ;
     int a = left, b = right;
     int key = num[left];
@@ -27,46 +43,76 @@ void quickSortPa(vector<int>& num, int left, int right) {
         if (a < b) num[b] = num[a];
     }
     num[a] = key;
-    quickSortPa(num, left, a - 1);
-    quickSortPa(num, a + 1, right);
+    quickSort(num, left, a - 1);
+    quickSort(num, a + 1, right);
 }
 
-void quickSort(vector<int>& num) {
-    quickSortPa(num, 0, num.size()-1);
+void QuickSort(vector<int>& num) {
+    quickSort(num, 0, num.size()-1);
 }
 
+// 归并排序
+void mergeSort(vector<int>& num, int left, int right) {
+    if (left >= right) return ;
+    int mid = (left + right) / 2;
+    mergeSort(num, left, mid);
+    mergeSort(num, mid+1, right);
+
+    int i = left, j = mid+1;
+    // vector<int> result;
+    int result[right-left+1], k = 0;
+    while (i <= mid && j <= right) {
+        if (num[i] <= num[j])
+            // result.push_back(num[i++]);
+            result[k++] = num[i++];
+        else
+            result[k++] = num[j++];
+    }
+    while (i <= mid) result[k++] = num[i++];
+    while (j <= right) result[k++] = num[j++];
+    for (int i = left, k = 0; i <= right; i++, k++)
+        num[i] = result[k];
+}
+
+void MergeSort(vector<int>& num) {
+    mergeSort(num, 0, num.size()-1);
+}
+
+// 计时
 void runTime(vector<vector<int>> nums, void (*sort)(vector<int>& num)) {
+    int size = nums.size();
     clock_t start, end, startPart;
     start = clock();
-    for (int i = 0; i < 1000; i++) {
-        startPart = clock();
+    for (int i = 0; i < size; i++) {
+        // startPart = clock();
         sort(nums[i]);
-        double curRunTime = (double)(clock() - startPart) / CLOCKS_PER_SEC;
+        // double curRunTime = (double)(clock() - startPart) / CLOCKS_PER_SEC;
         // cout << "Test" << i+1 << " n=" << nums[i].size() << ": " << curRunTime << endl;
-        cout << "Test" << i+1 << ": " << curRunTime << endl;
+        // cout << "Test" << i+1 << ": " << curRunTime << endl;
     }
     end = clock();
     double runTime = (double)(end - start) / CLOCKS_PER_SEC;
     cout << "Total: " << runTime << endl;
-    cout << "Average: " << runTime / 1000 << endl;
+    cout << "Average: " << runTime / size << endl;
 }
 
 int main() {
     srand(3);
-    vector<vector<int>> nums;
-    for (int i = 0; i < 1000; i++) {
-        vector<int> num;
-        for (int j = 0; j < 1000; j++) {
-            int n = proRand(0, 100);
-            num.push_back(n);
-        }
-        nums.push_back(num);
-    }
+    vector<int> n = {100, 200, 500, 1000, 2000, 5000, 10000};
 
-    cout << "Pub Sort" << endl;
-    runTime(nums, pubSort);
-    // cout << "Quick Sort" << endl;
-    // runTime(nums, quickSort);
+    // cout << "Bubble Sort" << endl;
+    // runTime(nums, bubbleSort);
+    cout << "Quick Sort" << endl;
+    // runTime(nums, QuickSort);
+    // cout << "Merge Sort" << endl;
+
+    for (int i = 0; i < n.size(); i++) {
+        vector<vector<int>> nums;
+        sampleData(nums, 1000, n[i]);
+        printf("T(%d)：\n", n[i]);
+        runTime(nums, QuickSort);
+        cout << "----" << endl;
+    }
 
     return 0;
 }
