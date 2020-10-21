9498ee4d03273f51b760ca4f350b1804543518f4
Shadowmaple <1142319190@qq.com>
2020-10-21 21:03:08 +0800

    test for lanqiao

---
+++ lanqiao/foo.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+# include <string>
+# include <sstream>
+using namespace std;
+
+int main() {
+	string s;
+	stringstream ss;
+	int a = 233;
+	cin >> a;
+	ss >> s;
+	cout << s << endl;
+	return 0;
+}
